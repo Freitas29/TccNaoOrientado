626c5330e5da8f3987c2105e0ecda3673789e60e
Freitas29 <arao.freitas@hotmail.com>
2018-06-28 12:52:53 -0300

    Fotos e bibliotecas add

--- HeaderLogado.php
+++ HeaderLogado.php
@@ -22,7 +22,7 @@ if((!isset ($_SESSION['Login']))){
 <link rel="stylesheet" href="./Materialize/css/materialize.css">
 
 <meta charset="utf-8">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="http://localhost/MaterialIcons-Regular.eot" rel="stylesheet">
 </head>
 
 <script>
@@ -50,6 +50,43 @@ $DadosUsuario = mysqli_query($oCon,$Usuario);
 ?>
 
 <style type="text/css">
+  @font-face {
+    font-family: 'Material Icons';
+    font-style: normal;
+    font-weight: 400;
+    src: url(iconfont/MaterialIcons-Regular.eot); /* For IE6-8 */
+    src: local('Material Icons'),
+      local('MaterialIcons-Regular'),
+      url(iconfont/MaterialIcons-Regular.woff2) format('woff2'),
+      url(iconfont/MaterialIcons-Regular.woff) format('woff'),
+      url(iconfont/MaterialIcons-Regular.ttf) format('truetype');
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;  /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: 'liga';
+}
+
   nav .input-field {
     margin: 0;
     height: auto !important;
--- Logado.php
+++ Logado.php
@@ -14,7 +14,7 @@
 	<link rel="stylesheet" href="./Materialize/css/materialize.css">
 
 	<meta charset="utf-8">
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="http://localhost/MaterialIcons-Regular.eot" rel="stylesheet">
 	</head>
 
 	<script>
@@ -82,6 +82,43 @@
 	</script>
 
 	<style>
+
+	@font-face {
+	  font-family: 'Material Icons';
+	  font-style: normal;
+	  font-weight: 400;
+	  src: url(iconfont/MaterialIcons-Regular.eot); /* For IE6-8 */
+	  src: local('Material Icons'),
+	    local('MaterialIcons-Regular'),
+	    url(iconfont/MaterialIcons-Regular.woff2) format('woff2'),
+	    url(iconfont/MaterialIcons-Regular.woff) format('woff'),
+	    url(iconfont/MaterialIcons-Regular.ttf) format('truetype');
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;  /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: 'liga';
+}
 
 	nav{
 		
@@ -209,7 +246,7 @@
 
 			include './conexoesPhp/Conexao.php';
 			
-			$ConteudoCelular = 'select ancTitulo,ancDesc,ancCodigo from anuncio  where ancCod_Categoria=1 and ancCod_Criador != '.$_SESSION['Login'].' and trocado = 0 order by ancCodigo desc limit 4';
+			$ConteudoCelular = 'select ancTitulo,ancDesc,ancCodigo from anuncio  where ancCod_Categoria=6 and ancCod_Criador != '.$_SESSION['Login'].' and trocado = 0 order by ancCodigo desc limit 4';
 			
 			$Usuario = 'select usrEmail,usrApelido,usrFoto from usuario where UsrCodigo='.$_SESSION['Login'];
 			
@@ -376,7 +413,7 @@
 			<div class="Header">
 			
 				<blockquote>
-					Celulares novos
+					Eletrodomésticos novos
 					<a href="#">Ver mais</a>
 				</blockquote>
 			
